6a8fecb0831a1b4a121175745c0ef2716cae74f8
Paul Boutot <paulvboutot@hotmail.com>
2020-02-02 18:04:09 -0500

    removed token

--- echobot.py
+++ echobot.py
@@ -1,14 +1,2 @@
 import slack
 
-# Echobot - OAuth and Permissions
-Bot_User_OAuth_Access_Token = 'xoxb-922258423332-922293102868-LrCDGJozUPM5GNPWyixUwmJA'
-SLACK_API_TOKEN = Bot_User_OAuth_Access_Token
-
-# Hardcoded SLACK_API_TOKEN
-slack_token = SLACK_API_TOKEN
-client = slack.WebClient(SLACK_API_TOKEN, timeout=30)
-
-# Echobot - Actions
-client.chat_postMessage(
-    channel='#general',
-    text='Hello World!')
